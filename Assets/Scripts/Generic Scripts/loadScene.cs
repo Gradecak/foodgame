4d73b21466da1b24aa6639d26db124541de8345c
Gradecak <gradecam@tcd.ie>
2016-08-14 11:49:17 +0200

    initial commit

---
+++ Assets/Scripts/Generic Scripts/loadScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour {
+
+	public void load(int scene_index){
+		SceneManager.LoadScene (scene_index);
+	}
+}
